8bf524158a67d9608c129e4ba6f1dab11068ac94
Whitney Muchira <whitneymuchira@gmail.com>
2023-02-14 08:44:03 +0300

    added eventListeners

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 
-
+let button=document.getElementById('button')
 
 const fetchAdvice=async()=>{
     const res= await fetch(`https://api.adviceslip.com/advice`)
     const data= await res.json()
     console.log(data)
 
-    document.getElementById('title').innerHTML=data.slip.id
-    document.getElementById('text').innerHTML=data.slip.advice
+    document.getElementById('title').innerHTML=`Advice # ${data.slip.id}`
+    document.getElementById('text').innerHTML=`"${data.slip.advice}"`
 }
+button.addEventListener('onClick', fetchAdvice)
 fetchAdvice()
 
